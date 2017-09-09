f8112b13445d362f699b44cf7eb25d59bd8b7058
Nat1405 <nathanc1945@gmail.com>
2017-09-08 14:51:36 -1000

    Made progress on object oriented routines and tasks

--- nifty/pipeline/objectoriented/subroutine.py
+++ nifty/pipeline/objectoriented/subroutine.py
@@ -1,15 +1,62 @@
 
 
-class Prepare(SubRoutine):
+class TaskRunner(object):
     """
-    Wrapper around nfprepare
+    Expects:
+
+    inputList: list of raw input files with no prefixes.
     """
-    def __init__(self):
-        """
-        Run a task; can be IRAF or Python.
+    @classmethod
+    def loop(self, task, inputList, skipMessage, prefix):
+        for item in inputList:
+            item = str(item).strip()
+            if os.path.exists(item):
+                if over:
+                    os.remove(item)
+                    task.run(item)
+                else:
+                    logging.info("Output exists and -over- not set - skipping " + str(skipMessage))
+            else:
+                task.run(item)
+        inputList = checkLists(inputList, '.', prefix, '.fits')
+        return inputList
 
-        """
-        pass
+    @classmethod
+    def combine(self, task, inputList, skipMessage, prefix, output):
+        if os.path.exists(output):
+            if over:
+                iraf.delete(output)
+                if len(inputList) > 1:
+                    task.run(listit(inputList, prefix))
+                else:
+                    iraf.copy(inputList[0], output)
+            else:
+                print "\nOutput exists and -over- not set - skipping " + str(skipMessage)
+        else:
+            if len(inputList) > 1:
+                task.run(listit(inputList,prefix)
 
-    def run(self, inputFile, **kwargs):
-        iraf.nfprepare(inputFile, **kwargs)
+    @classmethod
+    def single(self, task, inputList, skipMessage, prefix, output):
+        if os.path.exists(output):
+            if over:
+                iraf.delete(output)
+                task.run(listit(inputList, prefix))
+            else:
+                print "\nOutput exists and -over- not set - skipping " + str(skipMessage)
+        else:
+            if len(inputList) > 1:
+                task.run(listit(inputList,prefix)
+
+class Task(object):
+    """
+    Run like OP OOP!!!
+    """
+    def __init__(self, input_cls, **kwargs):
+        self.input_cls = input_cls
+        self.kwargs = kwargs
+    def run(self, *args):
+        kwargs = self.kwargs
+        self.input_cls(*args, **kwargs)
+
+Splot.run()
